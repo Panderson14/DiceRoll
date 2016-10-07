015cd2eeb27a3670c925516d54732bd3cf2bfda2
Patrick Anderson <psa5dg@virginia.edu>
2016-10-06 22:36:31 -0400

    Done-ish

---
+++ Dice Roll/CoinViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CoinViewController.swift
+//  Dice Roll
+//
+//  Created by Patrick Anderson on 10/6/16.
+//  Copyright © 2016 Patrick Anderson. All rights reserved.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+
+    @IBOutlet weak var Coin: UIImageView!
+    
+    
+    @IBAction func CoinFlip(_ sender: AnyObject) {
+        let flip = Int(arc4random_uniform(2) + 1)
+        
+        switch flip {
+        case 1:
+            Coin.image = UIImage(named:"Heads")
+        default:
+            Coin.image = UIImage(named:"Tails")
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
